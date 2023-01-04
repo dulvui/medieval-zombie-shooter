57021c6341fde75d9ced858a5b97eaca5f095ef4
Simon Dalvai <git@s9i.cc>
2023-01-04 16:25:50 +0100

    menu: more buttons

--- game/src/screens/menu/Menu.gd
+++ game/src/screens/menu/Menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
 
-func _on_Play_pressed():
+func _ready() -> void:
+	if OS.get_name() == "iOS":
+		$Buttons/Exit.hide()
+
+func _on_Play_pressed() -> void:
 	get_tree().change_scene("res://src/screens/game/Game.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
