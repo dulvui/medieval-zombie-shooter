82158c5f8af0868c1e30b76bf8bf78192a90bfc8
Simon Dalvai <git@s9i.cc>
2023-01-10 21:45:09 +0100

    fix points and arrows stick now to characters on hit

--- game/src/actors/armory/ammunition/arrow/Arrow.gd
+++ game/src/actors/armory/ammunition/arrow/Arrow.gd
@@ -1,23 +1,19 @@
 extends RigidBody2D
 
 const SPEED:float = 860.0
-onready var timer = $DissapearTimer
 var target
 
 func shoot(target:Vector2, power:float) -> void:
 	var direction = -(global_position - target).normalized()
 	apply_central_impulse(Vector2(direction.x, direction.y) * SPEED * power)
-	timer.start()
-
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
 
 
-func _on_DissapearTimer_timeout() -> void:
-	$AnimationPlayer.play("Dissapear")
-
-
 func _on_AnimationPlayer_animation_finished(anim_name) -> void:
 	queue_free()
 
+func _on_Area2D_area_entered(area):
+	$AnimationPlayer.play("Dissapear")
+	sleeping = true
